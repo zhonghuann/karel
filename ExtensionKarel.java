3ba8105293be11e8c5553f15d05187cf33d69630
S <s>
2017-01-16 15:18:50 +0800

    {"types":"A","runs":0}

---
+++ ExtensionKarel.java
@@ -0,0 +1,15 @@
+/*
+ * File: ExtensionKarel.java
+ * -------------------------------
+ * If you finish your assignment feel free to write any Karel
+ * program of your chosing. Writing an extension is not required
+ * but encouraged!
+ */
+
+import stanford.karel.*;
+
+public class ExtensionKarel extends SuperKarel {
+
+	// You fill in this part
+
+}
