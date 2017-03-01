534e051b68d9735ee8654776f30c017e03e9e8b6
S <s>
2017-03-01 13:32:39 +0800

    {"types":"C","runs":13}

--- MidpointFindingKarel.java
+++ MidpointFindingKarel.java
@@ -13,73 +13,142 @@
 import stanford.karel.*;
 
 public class MidpointFindingKarel extends SuperKarel {
-
 	public void run() {
-		fillFirstRow();	
-		goToOrigin();
-		//midpoint finding part
-		while (beepersPresent()) {		
-			pickBeeper();						
-			move();
-			moveToEnd();					
-		}		
+		fillFirstRow();
+		//find midpoint
+		while(beepersPresent()) {
+			secondRowAddOne();
+			collectLeftEnd();
+			if(frontIsClear()){
+				move();
+			}
+		}
+		goTomidpoint();
 		putBeeper();
+		collectSecondRow();
+		goToOrigin();
+		moveToBeeper();
 	}
-	
 	/*
 	 * fillFirstRow method is defined to fill the 1st row with beepers
 	 * Karel will put only one beeper at each corner
-	 * precondition: facing east, at (1,1) point
-	 * postcondition: facing east, at the end of 1st row
+	 * precondition: facing east, at(1,1) point
+	 * postcondition: facing east, at the most right end of 1st row
 	 */
-	private void fillFirstRow() {
-		goToOrigin(); //go to (1,1) facing east
-		while (frontIsClear()) {
+	private void fillFirstRow(){
+		goToOrigin();//go to (1,1) facing east
+		while(frontIsClear()) {
 			putBeeper();
 			move();
 		}
-		putBeeper(); //Off-By-One-Bug
+		putBeeper(); //off-by-one-bug
 	}
-	
 	/*
-	 * Karel will move to the last beeper of the beeper chain
-	 * Precondition: standing at one end facing that chain
-	 * Postcondition: standing at another end facing that chain 
-	 * which means the orientation is changed oppositely 
+	 * Collect the most left beeper on the 1st row, if there is any
+	 * Precondition: any case
+	 * Postcondition: facing east, stop at the position where Karel just collected one beeper, 1st row
+	 * 
 	 */
-	private void moveToEnd() {		
-		while ( beepersPresent() && frontIsClear() ) {
+	private void collectLeftEnd() {
+		goToOrigin();
+		moveToBeeper();
+		if(beepersPresent()) {
+			pickBeeper();
+			
+		}
+		if (frontIsClear()){
 			move();
 		}
-		if ( frontIsBlocked() && beepersPresent() ) { //for the case arrive at the wall
-			turnAround();
-		} else {
-			turnAround();
-			move();
+		if(beepersPresent()){
+			pickBeeper();
 		}
-		
 	}
-	
-	/*Move Karel back to origin of map (1,1), make it facing east
-	 * Precondition: can be any case
-	 * Postcondition: standing on (1,1) facing east
+	/*
+	 * go to 2nd row, add one more beeper
+	 * precondition: anywhere at 1st row facing east
+	 * postcondition: on 2nd row, at the most end where no more beeper exist
 	 */
-	private void goToOrigin() { 
-		while (notFacingSouth()) {
+	private void secondRowAddOne() {
+		goToOrigin();
+		bigTurnLeft();
+		turnRight();
+		while(beepersPresent()) {//arrive at the end of existing beepers
+			move();
+		}
+		putBeeper();
+	}
+	/*
+	 * Make sure Karel will go to the midpoint of the first row
+	 * precondition: any case
+	 * postcondition: arrive at the mid point of the 1st row, facing south
+	 */
+	private void goTomidpoint() {
+		goToOrigin();
+		moveToWall();
+		bigTurnLeft();
+		turnLeft();
+		moveToBeeper();
+		bigTurnLeft();
+	}
+	/*
+	 * collet the beepers on 2nd row
+	 */
+	private void collectSecondRow() {
+		goToOrigin();
+		bigTurnLeft(); //facing north
+		turnRight(); //facing east
+		while(beepersPresent() || frontIsBlocked()) {
+			pickBeeper();
+			move();
+		}
+	}
+	/*
+	 * Move Karel back to origin of map(1,1), make it facing east
+	 * precondition: can be any caxe
+	 * postcondition: standing on (1,1) facing east
+	 */
+	private void goToOrigin() {
+		while(notFacingSouth()) {
 			turnLeft();
 		}
 		moveToWall();
-		while (notFacingWest()) {
+		while(notFacingWest()){
 			turnLeft();
 		}
 		moveToWall();
 		while (notFacingEast()) {
-			turnLeft(); //Karel will face east
+			turnLeft(); //karel will face east
 		}
 	}
-	
-	private void moveToWall() { //move Karel to wall
-		while (frontIsClear()) {
+	/*
+	 * move Karel to first beeper on its way
+	 * precondition: facing east/west
+	 * postcondition: share same orientation as in precondition, standing on the corner where first beeper appears on its way
+	 */
+	private void moveToBeeper(){
+		while (noBeepersPresent()) {
+			move();
+		}
+	}
+	/*
+	 * go to upper/lower row above without changing column number
+	 * make a big turn left
+	 * precondition: any case
+	 * postcondition: same column, arrive at uper/lower row
+	 * which is one above/under the row in precondition
+	 * direction will turn left
+	 */
+	private void bigTurnLeft(){
+		turnLeft();
+		move();
+	}
+	/*
+	 * move Karel to the wall
+	 * precondition: any case
+	 * postcondition: near the wall
+	 */
+	private void moveToWall() {
+		while(frontIsClear()) {
 			move();
 		}
 	}
